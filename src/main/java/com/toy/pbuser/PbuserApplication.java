2b27d86c37273c6589a8ce56b4c638861c5e5e69
JongChan <kjc3590@gmail.com>
2021-06-06 19:34:43 +0900

    [Init] project init

---
+++ src/main/java/com/toy/pbuser/PbuserApplication.java
@@ -0,0 +1,13 @@
+package com.toy.pbuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbuserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toy/pbuser/PbuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.pbuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
