cd2a877af5545ec48556381919348b2011c3e945
JongChan <kjc3590@songareeit.com>
2021-06-07 15:55:19 +0900

    [Letter] Letter init

--- src/main/java/com/toy/pbuser/PbuserApplication.java
+++ src/main/java/com/toy/pbuser/PbuserApplication.java
@@ -2,8 +2,12 @@ package com.toy.pbuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@RefreshScope
+@EnableFeignClients
 public class PbuserApplication {
 
 	public static void main(String[] args) {
