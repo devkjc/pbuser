9657e25e20da2b37f2bae5849738f23c74ba8c54
JongChan <kjc3590@songareeit.com>
2021-06-16 18:50:32 +0900

    [Config] Zuul 세팅

--- src/main/java/com/toy/pbuser/PbuserApplication.java
+++ src/main/java/com/toy/pbuser/PbuserApplication.java
@@ -2,12 +2,14 @@ package com.toy.pbuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @RefreshScope
 @EnableFeignClients
+@EnableCircuitBreaker
 public class PbuserApplication {
 
 	public static void main(String[] args) {
