90cbc2775e380e71f61446f0e76148a96ccd5cb1
JongChan <kjc3590@gmail.com>
2021-07-20 22:45:13 +0900

    [Init] project security

--- src/main/java/com/toy/pbuser/config/security/SecurityConfig.java
+++ src/main/java/com/toy/pbuser/config/security/SecurityConfig.java
@@ -1,81 +1,109 @@
-//package com.toy.pbuser.config.security;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.toy.pbuser.config.security.models.SecurityProperties;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.web.AuthenticationEntryPoint;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.CorsConfigurationSource;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//
-//import java.sql.Timestamp;
-//import java.util.Arrays;
-//import java.util.Date;
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//@Configuration
-//@EnableWebSecurity
-//@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
-//@RequiredArgsConstructor
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    private final ObjectMapper objectMapper;
-//
-//    private final SecurityProperties restSecProps;
-//
-//    private final SecurityFilter tokenAuthenticationFilter;
-//
-//    @Bean
-//    public AuthenticationEntryPoint restAuthenticationEntryPoint() {
-//        return (httpServletRequest, httpServletResponse, e) -> {
-//            Map<String, Object> errorObject = new HashMap<String, Object>();
-//            int errorCode = 401;
-//            errorObject.put("message", "Unauthorized access of protected resource, invalid credentials");
-//            errorObject.put("error", HttpStatus.UNAUTHORIZED);
-//            errorObject.put("code", errorCode);
-//            errorObject.put("timestamp", new Timestamp(new Date().getTime()));
-//            httpServletResponse.setContentType("application/json;charset=UTF-8");
-//            httpServletResponse.setStatus(errorCode);
-//            httpServletResponse.getWriter().write(objectMapper.writeValueAsString(errorObject));
-//        };
-//    }
-//
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(Arrays.asList("*"));
-//        configuration.setAllowedMethods(restSecProps.getAllowedMethods());
-//        configuration.setAllowedHeaders(restSecProps.getAllowedHeaders());
-//        configuration.setAllowCredentials(restSecProps.isAllowCredentials());
-//        configuration.setExposedHeaders(restSecProps.getExposedHeaders());
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.cors().configurationSource(corsConfigurationSource()).and().csrf().disable().formLogin().disable()
-//                .httpBasic().disable().exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint())
-//                .and().authorizeRequests()
-//                .antMatchers(restSecProps.getAllowedPublicApis().stream().toArray(String[]::new)).permitAll()
-//                .antMatchers("/swagger-ui.html","/swagger-resources/**","/v2/api-docs", "/configuration/**", "/webjars/**", "/api-docs").permitAll()
-//                .antMatchers("/oauth/**","/csrf").permitAll()
+package com.toy.pbuser.config.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.toy.pbuser.config.security.models.SecurityProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final ObjectMapper objectMapper;
+    private final SecurityProperties restSecProps;
+    private final SecurityFilter tokenAuthenticationFilter;
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean       // 스프링 시큐리티에서 반환될 사용자 정보 저장
+    @Override
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return super.userDetailsServiceBean();
+    }
+
+    @Bean
+    public AuthenticationEntryPoint restAuthenticationEntryPoint() {
+        return (httpServletRequest, httpServletResponse, e) -> {
+            Map<String, Object> errorObject = new HashMap<String, Object>();
+            int errorCode = 401;
+            errorObject.put("message", "Unauthorized access of protected resource, invalid credentials");
+            errorObject.put("error", HttpStatus.UNAUTHORIZED);
+            errorObject.put("code", errorCode);
+            errorObject.put("timestamp", new Timestamp(new Date().getTime()));
+            httpServletResponse.setContentType("application/json;charset=UTF-8");
+            httpServletResponse.setStatus(errorCode);
+            httpServletResponse.getWriter().write(objectMapper.writeValueAsString(errorObject));
+        };
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        System.out.println("restSecProps :: " + restSecProps);
+
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(restSecProps.getAllowedMethods());
+        configuration.setAllowedHeaders(restSecProps.getAllowedHeaders());
+        configuration.setAllowCredentials(restSecProps.isAllowCredentials());
+        configuration.setExposedHeaders(restSecProps.getExposedHeaders());
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().configurationSource(corsConfigurationSource()).and().csrf().disable().formLogin().disable()
+                .httpBasic().disable().exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint())
+                .and().authorizeRequests()
+                .antMatchers(restSecProps.getAllowedPublicApis().stream().toArray(String[]::new)).permitAll()
+                .antMatchers("/swagger-ui.html","/swagger-resources/**","/v2/api-docs", "/configuration/**", "/webjars/**", "/api-docs").permitAll()
+                .antMatchers("/oauth/**","/csrf").permitAll()
 //                .antMatchers("/", "/hello","/profile","/api/v1/user/test").permitAll()
-//                .antMatchers("/actuator/**").permitAll()
-//                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll().anyRequest().authenticated().and()
-//                .addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//    }
-//}
+                .antMatchers("/actuator/**").permitAll()
+                .anyRequest().authenticated().and()
+                .addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder)
+                .withUser("user").password(passwordEncoder.encode("user1234")).roles("USER")
+                .and()
+                .withUser("admin").password(passwordEncoder.encode("admin1234")).roles("USER", "ADMIN");
+    }
+}
--- src/main/java/com/toy/pbuser/config/security/models/CookieProperties.java
+++ src/main/java/com/toy/pbuser/config/security/models/CookieProperties.java
@@ -1,12 +1,12 @@
-//package com.toy.pbuser.config.security.models;
-//
-//import lombok.Data;
-//
-//@Data
-//public class CookieProperties {
-//	private String domain;
-//	private String path;
-//	private boolean httpOnly;
-//	private boolean secure;
-//	private int maxAgeInMinutes;
-//}
+package com.toy.pbuser.config.security.models;
+
+import lombok.Data;
+
+@Data
+public class CookieProperties {
+	private String domain;
+	private String path;
+	private boolean httpOnly;
+	private boolean secure;
+	private int maxAgeInMinutes;
+}
