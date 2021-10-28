c15c8168a64b681e4c5125effcab3c809e3d364b
JongChan <kjc3590@songareeit.com>
2021-10-28 18:19:28 +0900

    [env] 개발서버로 이전

--- src/main/java/com/toy/pbuser/config/security/SecurityConfig.java
+++ src/main/java/com/toy/pbuser/config/security/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(restSecProps.getAllowedPublicApis().stream().toArray(String[]::new)).permitAll()
                 .antMatchers("/swagger-ui.html","/swagger-resources/**","/v2/api-docs", "/configuration/**", "/webjars/**", "/api-docs").permitAll()
                 .antMatchers("/oauth/**","/csrf").permitAll()
-                .antMatchers("/actuator/**").permitAll()
+                .antMatchers("**/actuator/**").permitAll()
                 .anyRequest().authenticated().and()
                 .addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
